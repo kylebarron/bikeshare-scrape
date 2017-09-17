8d6381fdc9a319814e2e060e0e0768182a1b9271
Kyle Barron <barronk@mit.edu>
2017-09-17 03:33:20 -0400

    working code to create initial tables in each bikeshare database

--- code/02create_dbs.py
+++ code/02create_dbs.py
@@ -1,67 +1,203 @@
 import requests
 import pandas as pd
 import pymysql
+import os
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData, Column, Table, ForeignKey
-from sqlalchemy import Integer, String, DateTime
 from sqlalchemy_utils import database_exists, create_database
-import os
+from sqlalchemy.dialects.mysql import \
+        BIGINT, BINARY, BIT, BLOB, BOOLEAN, CHAR, DATE, \
+        DATETIME, DECIMAL, DOUBLE, ENUM, FLOAT, INTEGER, \
+        LONGBLOB, LONGTEXT, MEDIUMBLOB, MEDIUMINT, MEDIUMTEXT, NCHAR, \
+        NUMERIC, NVARCHAR, REAL, SET, SMALLINT, TEXT, TIME, TIMESTAMP, \
+        TINYBLOB, TINYINT, TINYTEXT, VARBINARY, VARCHAR, YEAR
 
-password = open("/home/kyle/.config/mysql_kyle_passwd", 'r').read().splitlines()[0]
-test = create_engine('mysql+pymysql://kyle:' + password + '@localhost')
+main() 
 
-systems = pd.read_csv(os.path.join("..", "data", "gbfs_systems.csv"))
+def main():
+    password = open("/home/kyle/.config/mysql_kyle_passwd", 'r').read().splitlines()[0]
 
-# Create a database for each bikeshare provider within MySQL
-for i in systems['System ID']:
-    engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + i)
-    if not database_exists(engine.url):
-        create_database(engine.url)
+    systems = pd.read_csv(os.path.join("..", "data", "gbfs_systems.csv"))
 
-# Create a data frame of urls for every bikeshare provider
-url_df = pd.DataFrame()
-for i in range(1, len(systems)):
-    # First go to the system's gbfs site
-    gbfs = requests.get(systems['Auto-Discovery URL'][i]).json()
-    gbfs_urls = pd.io.json.json_normalize(gbfs['data']['en']['feeds'])
-    gbfs_urls['System ID'] = systems['System ID'][i]
-    url_df = url_df.append(gbfs_urls)
+    # Create a database for each bikeshare provider within MySQL
+    for i in systems['System ID']:
+        engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + i)
+        if not database_exists(engine.url):
+            create_database(engine.url)
+
+    # Create a data frame of urls for every bikeshare provider
+    url_df = pd.DataFrame()
+    for i in range(1, len(systems)):
+        # First go to the system's gbfs site
+        gbfs = requests.get(systems['Auto-Discovery URL'][i]).json()
+        gbfs_urls = pd.io.json.json_normalize(gbfs['data']['en']['feeds'])
+        gbfs_urls['System ID'] = systems['System ID'][i]
+        url_df = url_df.append(gbfs_urls)
+
+    # Remove all the rows with 'name' == 'gbfs' to prevent infinite recursion
+    url_df = url_df[url_df['name'] != 'gbfs']
     
-url_df
+    # Create tables
+    for i in range(1, len(url_df)):
+        # Make connection
+        engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + url_df['System ID'].tolist()[i])
+        if not engine.dialect.has_table(engine, url_df['name'].tolist()[i]):
+            create_table(url_df['name'].tolist()[i], password, url_df['System ID'].tolist()[i])
+
+def aside():    
+    # See how often data gets reloaded
+    ttl_df = pd.DataFrame()
+    for i in range(1, len(systems)):
+        ttl = requests.get(systems['Auto-Discovery URL'][i]).json()
+        ttl = pd.io.json.json_normalize(ttl)
+        ttl['System ID'] = systems['System ID'][i]
+        ttl_df = ttl_df.append(ttl[['System ID', 'ttl']])
+        
+    ttl_df[ttl_df['ttl'] != 60]
+    # All except Abu Dhabi have 60, Abu Dhabi has 10
+    # Therefore collecting data every minute is reasonable
+
+
+url_df 
+
+
+def create_table(type, password, system_id):
     
+    # if type == 'system_information':
+    #     engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + system_id)
+    #     metadata = MetaData(bind = engine)
+    #     table = Table(
+    #         'system_information',
+    #         metadata,
+    #         Column('last_updated', Timestamp, primary_key = True),
+    #         Column('system_id', VARCHAR(28)),
+    #         Column('language', VARCHAR(3)),
+    #         Column('timezone', VARCHAR(35))
+    #     )
+    #     metadata.create_all()
+    # 
+    # if type == 'station_information':
+    #     engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + system_id)
+    #     metadata = MetaData(bind = engine)
+    #     table = Table(
+    #         'station_information',
+    #         metadata,
+    #         Column('station_information_id', ),
+    #         Column('station_id', ),
+    #         Column('name', ),
+    #         Column('lat', DECIMAL(9,6)),
+    #         Column('lon', DECIMAL(9,6)),
+    #         Column('region_id', ),
+    #         Column('capacity', SmallInteger),
+    #         Column('eightd_has_key_dispenser', boolean),
+    #     )
+    #     metadata.create_all()
+    #     # - rental_methods	Optional	Array of enumerables containing the payment methods accepted at this station. 
     
-for i in 
-    engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + systems['System ID'][i])
-    metadata=MetaData(bind=engine)
-    main_table=Table('sample',metadata,
-                Column('LIN',String(10),primary_key=True),
-                Column('material_type',String(20),nullable=False),
-                Column('source',String(20),nullable=False),
-                Column('material_description',String(100)),
-                Column('quantity',Integer),
-                Column('location',String(2)),
-                Column('received_by',String(20)),
-                Column('received_date',DateTime,nullable=False),
-                )
-    metadata.create_all()
-    print(i)
+    if type == 'station_status':
+        engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + system_id)
+        metadata = MetaData(bind = engine)
+        table = Table(
+            'station_status',
+            metadata,
+            Column('id', BIGINT(unsigned = True), primary_key = True),
+            Column('station_id', SMALLINT(unsigned = True)),
+            Column('num_bikes_available', TINYINT(unsigned = True)),
+            Column('num_bikes_disabled', TINYINT(unsigned = True)),
+            Column('num_docks_available', TINYINT(unsigned = True)),
+            Column('num_docks_disabled', TINYINT(unsigned = True)),
+            Column('is_installed', BOOLEAN),
+            Column('is_renting', BOOLEAN),
+            Column('is_returning', BOOLEAN),
+            Column('last_reported', TIMESTAMP)
+        )
+        metadata.create_all()
+    
+    if type == 'free_bike_status':
+        engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + system_id)
+        metadata = MetaData(bind = engine)
+        table = Table(
+            'free_bike_status',
+            metadata,
+            Column('id', INTEGER(unsigned = True), primary_key = True),
+            Column('bike_id', SMALLINT(unsigned = True)),
+            Column('lat', DECIMAL(9,6)),
+            Column('lon', DECIMAL(9,6)),
+            Column('is_reserved', BOOLEAN),
+            Column('is_disabled', BOOLEAN),
+            Column('last_updated', TIMESTAMP)
+        )
+        metadata.create_all()
+
+    # if type == 'system_hours':
+    #     engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + system_id)
+    #     metadata = MetaData(bind = engine)
+    #     table = Table(
+    #     'system_hours',
+    #     metadata,
+    #     Column('id'),
+    #     Column('user_types'),
+    #     Column('days'),
+    #     Column('start_time'),
+    #     Column('end_time'),
+    #     
+    #     )
+    #     metadata.create_all()
+    
+    if type == 'system_calendar':
+        engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + system_id)
+        metadata = MetaData(bind = engine)
+        table = Table(
+            'system_calendar',
+            metadata,
+            Column('id', INTEGER(unsigned = True), primary_key = True),
+            Column('start_month', TINYINT(unsigned = True)),
+            Column('start_day', TINYINT(unsigned = True)),
+            Column('start_year', YEAR),
+            Column('end_month', TINYINT(unsigned = True)),
+            Column('end_day', TINYINT(unsigned = True)),
+            Column('end_year', YEAR)
+        )
+        metadata.create_all()
+
+    if type == 'system_regions': 
+        engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + system_id)
+        metadata = MetaData(bind = engine)
+        table = Table(
+            'system_regions',
+            metadata,
+            Column('id', INTEGER(unsigned = True), primary_key = True),
+            Column('region_id', SMALLINT(unsigned = True)),
+            Column('name', VARCHAR(50))
+        )
+        metadata.create_all()
+
+    # if type == 'system_pricing_plans':
+    #     engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + system_id)
+    #     metadata = MetaData(bind = engine)
+    #     table = Table(
+    #         'system_pricing_plans',
+    #         metadata,
+    #         Column('id', INTEGER(unsigned = True), primary_key = True),
+    #         Column('plan_id'),
+    #         Column('url'),
+    #         Column('name'),
+    #         Column('currency'),
+    #         Column('price'),
+    #         Column('is_taxable', BOOLEAN),
+    #         Column('description'),
+    #     )
+    #     metadata.create_all()
+
+    # if type == 'system_alerts':
+    #     engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/' + system_id)
+    #     metadata = MetaData(bind = engine)
+    #     table = Table(
+    #             'system_alerts',
+    #             metadata,
+    #             
+    #         )
+    #     metadata.create_all()
+    
 
 
-test2 = requests.get("https://gbfs.bcycle.com/bcycle_arborbike/gbfs.json")
-test2 = test2.json()
-test2 = pd.io.json.json_normalize(test2['data']['en']['feeds'])
-test2
-
-
-colnames
-test = gbfs.json() 
-pd.io.json.json_normalize(test['data']['en']['feeds'])
-list(systems.columns.values)
-
-
-engine = create_engine('mysql+pymysql://kyle:' + password + '@localhost/mydb')
-database_exists(engine.url)
-if not database_exists(engine.url):
-    create_database(engine.url)
-
-print(database_exists(engine.url))
