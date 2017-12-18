e489292e13c28a15a691dc323949c2b316c54a3c
Kyle Barron <barronk@mit.edu>
2017-12-17 23:34:39 -0500

    working code to update free_bike_status table

--- code/03gbfs_scrape.py
+++ code/03gbfs_scrape.py
@@ -6,6 +6,7 @@ import requests
 import time
 from pandas.io.json   import json_normalize
 from pandas.api.types import is_string_dtype
+from shapely.geometry import Point
 from sqlalchemy       import MetaData, Column, Table, ForeignKey
 from sqlalchemy       import create_engine
 from sqlalchemy.dialects.postgresql import \
@@ -32,6 +33,17 @@ def main():
         if url_df['name'][i] == 'station_status':
             try:
                 get_station_status(
+                    type = url_df['name'][i],
+                    url = url_df['url'][i],
+                    system_id = url_df['System ID'][i],
+                    password = password,
+                    port = port,
+                    start_time = start_time)
+            except Exception as e:
+                print(e)
+        elif url_df['name'][i] == 'free_bike_status':
+            try:
+                get_free_bike_status(
                     type = url_df['name'][i],
                     url = url_df['url'][i],
                     system_id = url_df['System ID'][i],
@@ -140,6 +152,81 @@ def get_station_status(
     print("Finished making engine connection %s seconds" % round(time.time() - start_time, 2))
 
     conn.execute(insert_stmt, toadd.to_dict('records'))
+    print("Finished inserting records into Postgresql %s seconds" % round(time.time() - start_time, 2))
+
+    conn.close()
+    print("--- Total time: %s seconds ---" % round(time.time() - start_time, 2))
+
+def get_free_bike_status(
+    type,
+    url,
+    system_id,
+    password,
+    port,
+    start_time):
+    
+    print('Attempting data retrieval for:\ntype = ' + type + '\nsystem_id = ' + system_id + '\nurl=' + url)
+
+    # Retrieve Data:
+    request = requests.get(url).json()
+    df = json_normalize(request['data']['bikes'])
+    df['last_updated'] = request['last_updated']
+    print("Downloaded free_bike_status %s seconds" % round(time.time() - start_time, 2))
+        
+    # Extract the number from bike_id if there's text in there
+    try:
+        df['bike_id'] = df['bike_id'].astype(int)
+    except ValueError:
+        match_list = df['bike_id'].str.findall(r'[0-9]+')
+        df['bike_id'] = match_list.str[0].astype(int)
+        print("Finished extracting number from bike_id %s seconds" % round(time.time() - start_time, 2))
+
+    # Now create id as numeric(string(station_id) + string(last_updated))
+    df['id'] = (
+        df['last_updated'].apply(str) +
+        df['bike_id'].apply(str)
+    ).apply(int)
+
+    # Put last_updated in datetime format
+    # Replace NaN with 1 for the timestamps
+    df['last_updated'] = df['last_updated'].fillna(value = 1)
+    try:
+        df['last_updated'] = pd.to_datetime(df['last_updated'], unit = 's')
+    except:
+        print(system_id + ' felt like using milliseconds')
+        df['last_updated'] = pd.to_datetime(df['last_updated'], unit = 'ms')
+        df['last_updated'] = df['last_updated'] / 1000
+        df['last_updated'].fillna(0, inplace = True)
+        df['last_updated'] = df['last_updated'].astype(int)
+
+    df['lat_lon'] = 'SRID=4326;' + df[['lon', 'lat']].apply(Point, axis = 1).astype(str)
+    
+    df = df.drop(columns = ['name', 'lat', 'lon'])
+    
+    df.is_reserved = df.is_reserved.astype(bool)
+    df.is_disabled = df.is_disabled.astype(bool)
+    
+    engine = create_engine('postgresql+psycopg2://kyle:' + password + '@localhost:' + port + '/bikeshare')
+    print("Finished creating engine %s seconds" % round(time.time() - start_time, 2))
+
+    metadata = MetaData(bind = engine, schema = system_id)
+    table = Table(
+        'free_bike_status',
+        metadata,
+        Column('id', BIGINT, primary_key = True),
+        Column('bike_id', SMALLINT),
+        Column('lat_lon', Geometry(geometry_type='POINT', srid=4326)),
+        Column('is_reserved', BOOLEAN),
+        Column('is_disabled', BOOLEAN),
+        Column('last_updated', TIMESTAMP))
+    
+    insert_stmt = insert(table).on_conflict_do_nothing(index_elements = ['id'])
+    # To check the SQL generated is correct: print(str(insert_stmt))
+    
+    conn = engine.connect()
+    print("Finished making engine connection %s seconds" % round(time.time() - start_time, 2))
+
+    conn.execute(insert_stmt, df.to_dict('records'))
     print("Finished inserting records into Postgresql %s seconds" % round(time.time() - start_time, 2))
 
     conn.close()
